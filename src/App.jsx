328dec5834984f63e6f504df8c79cc9d6268e9b8
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-03-25 22:53:14 +1000

    js logic in components

--- src/App.jsx
+++ src/App.jsx
@@ -74,6 +74,25 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  if (hour >= openHour && hour <= closeHour) {
+    console.log("We're open");
+  } else {
+    console.log(`Sorry we are closed.`);
+  }
+
+  // or
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  if (isOpen) {
+    console.log("We're open");
+  } else {
+    console.log(`Sorry we are closed.`);
+  }
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We are currently open!</footer>
   );
