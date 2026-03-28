3124b317f822604c4d896de94b0ba4c5bebc9f27
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-03-28 12:43:32 +1000

    conditionally renering with ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -67,12 +67,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We’re still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -80,7 +82,7 @@ function Menu() {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 9;
+  const openHour = 12;
   const closeHour = 22;
 
   if (hour >= openHour && hour <= closeHour) {
@@ -100,11 +102,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order ">
           <p>We’re open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We’re happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );
