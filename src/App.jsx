39be8509bde4a711fb3466aac540282c660d1f92
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-03-29 23:22:34 +1000

    react fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -68,11 +69,17 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We’re still working on our menu. Please come back later :)</p>
       )}
