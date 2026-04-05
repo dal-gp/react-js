2538503d1465c8ed8fbeb57ea9d667f2cb28c11b
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-05 13:12:02 +1000

    building a form and handling submissions

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,25 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip? </h3>
-    </div>
+      <select>
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option> */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
