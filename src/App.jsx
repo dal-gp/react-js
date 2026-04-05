211ab7a38e7b9355b12dad30bdd6d349bd5db36d
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-05 21:33:34 +1000

    controlled elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const intialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,13 +22,25 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { quantity, description, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setQuantity(1);
+    setDescription("");
   }
   return (
     <form onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip? </h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {/* <option value={1}>1</option>
         <option value={2}>2</option>
         <option value={3}>3</option> */}
@@ -36,7 +50,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
