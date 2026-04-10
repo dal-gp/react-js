e14835b71f06ea0e31fbe83714bb0b92567301e9
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-10 12:57:53 +1000

    deleting an item - more child to parent communication

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
 
-const intialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
-
 function App() {
   const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -69,22 +67,22 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <ul>
       {items.map((item) => (
-        <Item item={item} key={item.id} />
+        <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
       ))}
     </ul>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
-        <button>❌</button>
+        <button onClick={() => onDeleteItem(item.id)}>❌</button>
       </span>
     </li>
   );
