20f1ab933832e96c34a36033aa43fb018ac88cd0
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-12 23:44:26 +1000

    calculating statistics as dervied state

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,11 +111,36 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p>
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPackedItems = items.filter((item) => item.packed === true).length;
+  const percentage = Math.round((numPackedItems / numItems) * 100);
   return (
     <footer>
       <p>
-        <em>👝 You have X items on your list, and you already packed X(x%)</em>
+        {/* <em>
+          {percentage === 100
+            ? "You have everything! Ready to go ✈️ "
+            : `👝 You have ${numItems} items on your list, and you already packed
+          ${numPackedItems} (${percentage}%)`}
+        </em> */}
+
+        {percentage === 100 ? (
+          <em>You have everything! Ready to go ✈️</em>
+        ) : (
+          <em>
+            👝 You have {numItems} items on your list, and you already packed
+            {numPackedItems} ({percentage}%)
+          </em>
+        )}
       </p>
     </footer>
   );
