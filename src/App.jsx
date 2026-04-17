7e1ef168aab1445f455b763f0d9bbabe4d919586
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-17 12:13:52 +1000

    more reusability with the children prop

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,7 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p>
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
             <span>👈</span>Previous
           </Button>
@@ -41,6 +39,15 @@ function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div>
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
