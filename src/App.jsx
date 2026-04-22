50052a6bcd3bebbf215272d2f057acd58926a2a2
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-04-22 10:56:02 +1000

    splitting components in practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,230 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+
+const tempMovieData = [
+  {
+    imdbID: "tt137566",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <NavBar />
+      <Main />
     </>
-  )
+  );
 }
 
-export default App
+function NavBar() {
+  return (
+    <nav>
+      <Logo />
+      <Search />
+      <Numresults />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search() {
+  const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <input
+      placeholder="Search movie..."
+      type="text"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+  );
+}
+
+function Numresults() {
+  return (
+    <p>
+      Found <strong>3</strong> results
+    </p>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+}
+
+function ListBox() {
+  const [isOpenMovies, setIsOpen] = useState(true);
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen((isOpenMovies) => !isOpenMovies)}>
+        {isOpenMovies ? "-" : "+"}
+      </button>
+      {isOpenMovies && <MovieList />}
+    </div>
+  );
+}
+
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt="movie.Title" />
+      <h3>{movie.Title}</h3>
+      <p>
+        <span>🗓️</span>
+        <span>{movie.Year}</span>
+      </p>
+    </li>
+  );
+}
+
+function WatchedBox() {
+  const [isOpenWatched, setIsOpenWatched] = useState(true);
+  const [watchedMovies, setWatchedMovies] = useState(tempWatchedData);
+  return (
+    <div>
+      <button
+        onClick={() => setIsOpenWatched((isOpenWatched) => !isOpenWatched)}
+      >
+        {isOpenWatched ? "-" : "+"}
+      </button>
+      {isOpenWatched && (
+        <div>
+          <WatchedSummary watchedMovies={watchedMovies} />
+          <WatchedMovieList watchedMovies={watchedMovies} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function WatchedSummary({ watchedMovies }) {
+  const averageUserRating =
+    watchedMovies.reduce((accu, movie) => accu + movie.userRating, 0) /
+    watchedMovies.length;
+
+  const averageIMDBRating =
+    watchedMovies.reduce((accu, movie) => accu + movie.imdbRating, 0) /
+    watchedMovies.length;
+
+  const averageRuntime =
+    watchedMovies.reduce((accu, movie) => accu + movie.runtime, 0) /
+    watchedMovies.length;
+
+  return (
+    <div>
+      <h2>Movies you watched</h2>
+      <p>
+        <span>#️⃣</span>
+        <span>2 movies</span>
+      </p>
+      <p>
+        <span>⭐</span>
+        <span>{averageIMDBRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{averageUserRating}</span>
+      </p>
+      <p>
+        <span>⌛</span>
+        <span>{averageRuntime} min</span>
+      </p>
+    </div>
+  );
+}
+
+function WatchedMovieList({ watchedMovies }) {
+  return (
+    <ul>
+      {watchedMovies.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function WatchedMovie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt="" />
+      <p>{movie.Title}</p>
+      <p>
+        <span>⭐</span>
+        <span>{movie.userRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{movie.imdbRating}</span>
+      </p>
+      <p>
+        <span>⌛</span>
+        <span>{movie.runtime}</span>
+      </p>
+    </li>
+  );
+}
+export default App;
