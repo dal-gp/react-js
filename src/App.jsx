dbde82b469f436f1765be285064dfbb3c4283c03
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-05-20 14:07:21 +1000

    prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -46,20 +46,21 @@ const tempWatchedData = [
   },
 ];
 function App() {
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 }
 
-function NavBar() {
+function NavBar({ movies }) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={movies} />
     </nav>
   );
 }
@@ -87,31 +88,30 @@ function Search() {
   );
 }
 
-function NumResults() {
-  return <p>Found X results</p>;
+function NumResults({ movies }) {
+  return <p>Found {movies.length} results</p>;
 }
 
-function Main() {
+function Main({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
   );
 }
 
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div>
       <button onClick={() => setIsOpen((p) => !p)}>{isOpen ? "-" : "+"}</button>
-      {isOpen && <MovieList />}
+      {isOpen && <MovieList movies={movies} />}
     </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
   return (
     <ul>
       {movies.map((movie) => (
